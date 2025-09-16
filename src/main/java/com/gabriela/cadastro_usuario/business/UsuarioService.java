2cf603e40ee6d84f6d6d40ce52ca05a2ca23cab4
gabrielaoliveira <gabiof121@gmail.com>
2025-09-16 11:45:28 -0300

    sd

--- src/main/java/com/gabriela/cadastro_usuario/business/UsuarioService.java
+++ src/main/java/com/gabriela/cadastro_usuario/business/UsuarioService.java
@@ -28,5 +28,15 @@ public class UsuarioService {
         repository.deleteByEmail(email);
     }
 
+    public void atualizarUsuarioPorEmail(Integer id, Usuario usuario){
+        Usuario usuarioEntity=repository.findById(id).orElseThrow(() ->
+                new RuntimeException("Usuario nao encontrado"));
+        Usuario usuarioAtualizado= Usuario.builder()
+                .email(usuario.getEmail() != null ? usuario.getEmail() :
+                        usuarioEntity.getEmail())
+                .nome(usuario.getNome() != null ? usuario.getNome() :
+                        usuarioEntity.getNome())
+                .id(usuarioEntity.getId())
+                .build();
 
 }
